8769903678b47243c57d3ad6e867c0c774f4795d
cyrils <nigthguarder@gmail.com>
2023-12-05 20:54:13 +0100

    Assets + app structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.studycards"
         minSdk = 24
+        //noinspection OldTargetApi already the newest...
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -54,6 +55,7 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
     //Compose dependency
+    //noinspection GradleDependency
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
